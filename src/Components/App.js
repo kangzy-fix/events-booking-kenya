4b4bc7ae9efc57ee12aa58bd823ea662262d7713
Grace Aluoch <gracedeborah377@gmail.com>
2022-12-06 10:42:18 +0300

    add app component

---
+++ src/Components/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import {Route, Routes } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Header from "./Header";
+
+
+function App (){
+  const [ user, setUser] = useState(null);
+
+  useEffect( () => {
+    //auto log in
+    fetch("/users").then((r) => {
+      if (r.ok){
+        r.json().then((user) => setUser(user));
+      }
+    });
+  },[]);
+
+    return (
+      <>
+        <div className="banner_title">
+          <Header/>
+        </div>
+        <div class="banner">
+          <div class="mini">
+            <NavBar user={user} setUser={setUser} />
+          </div>
+        </div>
+
+        <main>
+        {user ? (
+          <Routes>
+            <Route path="/" element = {<Home user={user}/>} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/signup" element = { <SignUp setUser={setUser} />} />
+            <Route path="/login" element = {<Login  setUser={setUser} />}  />
+            <Route path="/" element = {<Home />} />
+          </Routes>
+        )}
+      </main>
+      </>
+    );
+};
+export default App;
