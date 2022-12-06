057e205ee66e9b789731cb345c2bc7810ba0bacc
Meshack <mbuvi.meshack99@gmail.com>
2022-12-06 10:31:38 +0300

    added fetch and state to signup form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
 
 function SignUp({ setUser }) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [msg,setMsg] = useState();
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
   
-    
+    function handleSubmit(e) {
+      e.preventDefault();
+      fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+          password_confirmation: passwordConfirmation,
+        }),
+      }).then((r) => {
+        if (r.ok) {
+          r.json().then((user) => setUser(user));
+        }else
+        setMsg("*Password should be identical and username unique")
+       
+  
+      });
+    }
   return (
     <div className="form_container">
       <form className="form_signup" onSubmit={handleSubmit}>
